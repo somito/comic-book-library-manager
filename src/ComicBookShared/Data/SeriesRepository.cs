a279caffce486b79876a56662390893dd0f93da3
Soma <Soma@SOMA-PC>
2017-06-14 20:05:32 +0200

    finished the todos in the series controller and created series repository.

---
+++ src/ComicBookShared/Data/SeriesRepository.cs
@@ -0,0 +1,45 @@
+﻿using ComicBookShared.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookShared.Data
+{
+    public class SeriesRepository : BaseRepository<Series>
+    {
+        public SeriesRepository(Context context)
+            : base(context)
+        {
+        }
+
+        public override IList<Series> GetList()
+        {
+            return Context.Series
+                    .OrderBy(s => s.Title)
+                    .ToList();
+        }
+
+        public override Series Get(int? id, bool includeRelatedEntities = true)
+        {
+            var series = Context.Series.AsQueryable();
+
+            if (includeRelatedEntities)
+            {
+                series = series.Include(s => s.ComicBooks);
+            }
+            
+            return series
+                .Where(s => s.Id == id)
+                .SingleOrDefault();
+        }
+
+        public bool Validate(Series series)
+        {
+            return Context.Series.Any(s => s.Id != series.Id &&
+                                                  s.Title == series.Title);
+        }
+    }
+}
